5e4ef38b18952c1bd005df02d143f0ec6046f5c7
Sokolov Lev Sergeevich <89748410+levs16@users.noreply.github.com>
2023-10-02 18:28:34 +0300

    Update MakeItGo.py

--- gitmig/MakeItGo.py
+++ gitmig/MakeItGo.py
@@ -1,26 +1,26 @@
-from PyQt5.QtWidgets import QWidget, QTextBrowser, QPushButton, QLineEdit
-
-code = {
-    "a": ".-",    "b": "-...",    "c": "-.-.",    "d": "-..",    "e": ".",    "f": "..-.",    "g": "--.",    "h": "....",    "i": "..",    "j": ".---",    "k": "-.-",    "l": ".-..",    "m": "--",    "n": "-.",    "o": "---",    "p": ".--.",    "q": "--.-",    "r": ".-.",    "s": "...",    "t": "-",    "u": "..-",    "v": "...-",    "w": ".--",    "x": "-..-",    "y": "-.--",    "z": "--.."}
+from PyQt5.QtWidgets import QWidget, QTextBrowser, QPushButton, QApplication
 
 
-class MorseCode(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Азбука Морзе 2")
-        self.setGeometry(300, 300, 330, 100)
-        alphabet_buttons = dict()
+class Suffle(QWidget):
+    def init(self):
+        super().init()
+        self.initUI()
 
-        for i, key in enumerate(list(code.keys())):
-            btn = QPushButton(key, self)
-            btn.resize(20, 20)
-            btn.move(10 + (i * 20) % 300, 10 + ((i * 20) // 300) * 20)
-            btn.clicked.connect(self.add_code)
-            alphabet_buttons[key] = btn
-        self.result = QLineEdit(self)
-        self.result.move(10, 50)
-        self.result.resize(280, 20)
-        self.alphabet_buttons = alphabet_buttons
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 330)
+        self.setWindowTitle('Перемешивание:')
+        self.button = QPushButton('Загрузить строки', self)
 
-    def add_code(self):
-        self.result.setText(self.result.text() + code[self.sender().text()])
+        self.button.move(10, 10)
+        self.text_field = QTextBrowser(self)
+
+        self.text_field.move(10, 40)
+        self.text_field.resize(280, 280)
+
+        self.button.clicked.connect(self.load_strings)
+
+    def load_strings(self):
+        with open('lines.txt', encoding='utf-8') as f:
+            text = f.read().strip().split('\n')
+        self.text_field.setText(
+            "\n".join(text[1::2]) + "\n" + "\n".join(text[::2]))
